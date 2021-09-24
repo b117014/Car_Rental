d02377ae74683b10fca223454690fc7e249e8009
b117014 <b117014@iiit-bh.ac.in>
2021-09-24 13:45:50 +0530

    integrated api in dashboard page

--- client/src/view/UserDashboardView/UserDashboardContainer.js
+++ client/src/view/UserDashboardView/UserDashboardContainer.js
@@ -1,7 +1,17 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from "react";
 import UserDashboardContent from "./UserDashboardContent";
+import { useDispatch } from "react-redux";
+import { userVehicleBookingSlotGetAction } from "../../_redux/action/slot";
 
 const UserDashboardContainer = () => {
+  const dispatch = useDispatch();
+  const onGetUserSlotBooking = async () => {
+    await dispatch(userVehicleBookingSlotGetAction());
+  };
+  useEffect(() => {
+    onGetUserSlotBooking();
+  }, []);
   return <UserDashboardContent />;
 };
 
